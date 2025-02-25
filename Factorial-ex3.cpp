41193b7856626fc3fdcf4aef6377269b3e4b3130
Morgan Smith <mtsmith8@terpmail.umd.edu>
2025-02-25 14:31:36 -0500

    Ex3 Complete

--- Factorial-ex3.cpp
+++ Factorial-ex3.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-int factorial(int n) {
-    for (int i = n - 1; i > 1; i--) {
-        n = n * i;
-    }
-    return n;
-}
 
 int main() {
     for (int i = 1; i <= 10; i++) {
-        std::cout << i << "! = " << factorial(i) << '\n';
+        int n = i;
+        for (int j = n - 1; j > 1; j--) {
+            n = n * j;
+        }
+        std::cout << i << "! = " << n << '\n';
     }
 }
