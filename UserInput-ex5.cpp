37fbfb03c521d3c3f7e70b50f897045f6de31dd7
Morgan Smith <mtsmith8@terpmail.umd.edu>
2025-02-25 14:38:44 -0500

    Ex5 Complete

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,16 @@
-// Add your code here
+#include <iostream>
+
+int factorial(int n) {
+    for (int i = n - 1; i > 1; i--) {
+        n = n * i;
+    }
+    return n;
+}
+
+int main() {
+    int n;
+    std::cout << "Please enter an integer value: [N]\n";
+    std::cin >> n;
+    std::cout << "The value you entered is " << n << " and its factorial is "
+              << factorial(n) << '\n';
+}
